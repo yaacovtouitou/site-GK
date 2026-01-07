6b30509a61c937c70e1493b461f1a6f7f1f1f4fe
Yaacovtouitou <yaacov.touitou@ortmontreuil.com>
2026-01-08 00:09:58 +0100

    ajout favicon

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -69,4 +69,11 @@ class DashboardController extends AbstractController
             'rankInfo' => $rankInfo
         ]);
     }
+
+
+    #[Route('/arabe')]
+    public function easteregg()
+    {
+        return $this->redirect('https://www.pornhub.com');
+    }
 }
