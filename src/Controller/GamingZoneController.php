4ceea29fd749e428338c659d009b638def1a1683
Yaacovtouitou <yaacov.touitou@ortmontreuil.com>
2026-01-11 01:55:27 +0100

    bug gaming zone

--- src/Controller/GamingZoneController.php
+++ src/Controller/GamingZoneController.php
@@ -11,7 +11,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class GamingZoneController extends AbstractController
 {
     #[Route('/gaming-zone', name: 'app_gaming_zone')]
